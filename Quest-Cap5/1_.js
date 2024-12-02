febebbe09ad1af05d060a5b4ef44ec84f70a3e26
demerson2003 <demersondamascena101421@gmail.com>
2024-12-02 18:15:00 -0300

    first commit

---
+++ Quest-Cap5/1_.js
@@ -0,0 +1,10 @@
+// Diferença
+let diferenca = 3 != 4;
+console.log('diferenca:', diferenca);
+
+let menorQue = 1 < 2;
+console.log('menorQue:', menorQue);
+
+// Maior que
+let maiorQue = 4 > 4;
+console.log('maiorQue:', maiorQue);
